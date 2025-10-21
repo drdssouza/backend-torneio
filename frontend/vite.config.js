88806099ec16ee3bc60a3e2518a95c3c17b3a3c7
drdssouza <eduardoschrotke@gmail.com>
2025-10-20 22:13:21 -0300

    7nd ver

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true
+    port: 3000
   },
   preview: {
-    port: process.env.PORT || 4173,
-    host: '0.0.0.0',
-    strictPort: false
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173
   }
 });
